95437c4cd47da77f82eafaf8df814f44a5ad7293
pdd <17683992872@163.com>
2023-04-28 19:41:33 +0800

    修改沙漏

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -15,7 +15,7 @@ export default hopeTheme({
     name: 'Mr.Pdd',
     url: 'https://tzwtmll.github.io',
   },
-  iconAssets: '//at.alicdn.com/t/c/font_4044544_l317cgmgy0c.css',
+  iconAssets: '//at.alicdn.com/t/c/font_4044544_dd336mwndyp.css',
   logo: '/assets/svg/logo.svg',
   repo: 'https://github.com/tzwtmll/docs',
   docsDir: 'demo/theme-docs/src',
